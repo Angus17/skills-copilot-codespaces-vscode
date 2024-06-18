a61b647afe716fa433aeb9f628d4e320138c23c3
Angus17 <105767789+Angus17@users.noreply.github.com>
2024-06-18 20:56:23 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,15 @@
+function skillsMember() {
+  const skills = [
+    'HTML',
+    'CSS',
+    'JavaScript',
+    'React',
+    'Node.js',
+    'Express',
+    'MongoDB',
+    'SQL',
+    'Python',
+    'Django',
+  ];
+  return skills;
+}
